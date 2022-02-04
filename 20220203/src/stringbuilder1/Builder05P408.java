1b7f40b05a7c3de7e97348d94db25a9b8e0ce072
limbbbyoung <klk2254@naver.com>
2022-02-04 21:40:33 +0900

    StringBuilder, Mayh, Arrays

--- 20220203/src/stringbuilder1/Builder05P408.java
+++ 20220203/src/stringbuilder1/Builder05P408.java
@@ -5,16 +5,15 @@ public class Builder05P408 {
 	public static void main(String[] args) {
 		// 다음 문자열이 문자를 0번부터 시작해서 3번째마다 삭제해주세요.
 		// 힌트 : 반복문을 쓰세요.
-		StringBuilder a = new StringBuilder("abcdeabcdeabcdeabcdeabcde");
-
-        for(int i = 0; i<a.length(); i++) {
-    		int count = 0;
-	     if(i == count) { 
-        	a.deleteCharAt(count);
-        	count = count + 2;
-        } else { 
-        	System.out.print(a.charAt(i)); 
+		StringBuilder a = new StringBuilder("abcdefghijklmnopqrstuvWxyz");
+        System.out.println(a);
+        int checkNum = a.length() % 3;
+        System.out.println(checkNum);
+        
+        int loopNum = a.length() - checkNum;
+        
+        for(int i = loopNum; i > -1;i--) {
+        	
         }
-	} 
 	}
 }
